4ff7532d5607f37f822cc8d3804b16139b842d91
Owen Zhang <owen.zhang@emc.com>
2018-07-12 15:49:09 +0800

    resolve 0011

---
+++ zxw-python-code/0011/0011.py
@@ -0,0 +1,34 @@
+
+class Input(object):
+    def __init__(self):
+        self.filter_words = list()
+        self.in_strings = ''
+        self.out_strings = 'Human '
+        self.load_filter_words()
+
+
+    def load_filter_words(self, filename='filter_words.txt'):
+        with open(filename, 'r', encoding='utf8') as file:
+            for line in file:
+                self.filter_words.append(line.strip())
+
+
+    def user_input(self):
+        self.in_strings = input('>')
+
+    def std_output(self):
+        self.filter_word()
+        print(self.out_strings)Rights
+
+    def filter_word(self):
+        for word in self.filter_words:
+            if self.in_strings.find(word) != -1:
+                self.out_strings = 'Freedom'
+                return
+
+
+
+
+i = Input()
+i.user_input()
+i.std_output()
