e24fc2e8e53d955ff700dd0dfc907f2bfaf2d8f5
Owen Zhang <owen.zhang@emc.com>
2018-07-19 16:48:48 +0800

    resolve 0015

---
+++ zxw-python-code/0015/0015.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''第 0015 题： 纯文本文件 city.txt为城市信息, 里面的内容（包括花括号）如下所示：
+{
+    "1" : "上海",
+    "2" : "北京",
+    "3" : "成都"
+}
+请将上述内容写到 city.xls 文件中。'''
+
+from openpyxl import Workbook
+import json
+
+def txt_to_xls(filename):
+    file = open(filename,'r', encoding='utf-8')
+    file_cintent = json.load(file, encoding='utf-8')
+    wb = Workbook()
+    sheet = wb.active
+    sheet.title = 'Citys'
+    for i in range(1,len(file_cintent)+1):
+        # sheet.cell(row=i, column = 1).value = i
+        print(file_cintent)
+        list = file_cintent.get(str(i))
+        j = 1
+        if callable(list):
+            for cell in list:
+                sheet.cell(row=i, column=j).value = cell
+                j += 1
+        else:
+            sheet.cell(row=i, column=1).value = list
+
+
+    wb.save('city.xlsx')
+
+
+txt_to_xls('city.txt')
